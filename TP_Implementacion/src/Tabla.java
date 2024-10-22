33b68ce87be38668b91ded36050e0c610c887a84
Julian Rieznik <julianrieznik@gmail.com>
2024-10-22 12:21:17 -0300

    Constructor Tabla c/ etiquetas numeradas

--- TP_Implementacion/src/Tabla.java
+++ TP_Implementacion/src/Tabla.java
@@ -11,9 +11,42 @@ public class Tabla{
         tabla = new LinkedHashMap<>();
         etiquetas_fila = new ArrayList<>();
     }
+
+    //Constructor sin etiqueas de filas, numeradas
+    public <E> Tabla(E[] etiquetaColumnas, E[][] columnas) {
+        this();
+
+        if (etiquetaColumnas.length != columnas.length) {
+            //HACER EXCEPCION PROPIA
+            throw new IllegalArgumentException("La cantidad de etiquetas de columnas debe coincidir con la cantidad de columnas.");
+        }
+        
+        //Inicializar etiquetas numeradas
+        for (int i = 0; i < columnas[0].length; i++){
+            Etiqueta<Integer> etiqueta = new Etiqueta<>(i);
+            etiquetas_fila.add(etiqueta);
+        }
+
+
+    
+        // Inicializar columnas asociadas a etiquetas de columnas
+        for (int i = 0; i < etiquetaColumnas.length; i++) {
+            Etiqueta<E> etiquetaColumna = new Etiqueta<>(etiquetaColumnas[i]);
+            
+            // Crear  Columna para la etiqueta actual
+            Columna<E> columna = new Columna<>(columnas[i]);
+            
+            // Agregar la columna a la tabla
+            tabla.put(etiquetaColumna, columna);
+        }
+    }
     public <E> Tabla(E[] etiquetaFilas, E[] etiquetaColumnas, E[][] columnas) {
         this();
-    
+
+        if (etiquetaColumnas.length != columnas.length) {
+            //HACER EXCEPCION PROPIA
+            throw new IllegalArgumentException("La cantidad de etiquetas de columnas debe coincidir con la cantidad de columnas.");
+        }
 
         for (E elemento : etiquetaFilas){
             Etiqueta<E> etiqueta = new Etiqueta<>(elemento);
