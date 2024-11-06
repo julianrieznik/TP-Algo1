5ac2a918ec4fc85f3c6b1941fba1fde5aff3ed5c
tomassirne <tomassirne@gmail.com>
2024-11-06 15:48:03 -0300

    Excepciones de Subtabla

--- TP_Implementacion/src/interfaces/Proyectable.java
+++ TP_Implementacion/src/interfaces/Proyectable.java
@@ -3,6 +3,8 @@ import java.util.List;
 
 public interface Proyectable<T,K,F> {
     T subtabla(List<F> listFilas, List<K> listColumnas);
+    T subtablaFilas(List<F> listFilas);
+    T subtablaColumnas(List<K> listColumnas);
     T head(int n);
     T tail(int n);
 }
